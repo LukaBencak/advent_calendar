06b39d5449147e3a70c4066ac84f0c137810f6fb
LukaBencak <79459988+LukaBencak@users.noreply.github.com>
2023-10-19 22:35:20 +0200

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
-let clicks = 0;
-function generate() {
-	clicks += 1;
+$(function (){
 
-	if (clicks <= 3) {
-		var date = ["Picnic in a car", "Road trip (destinacija neznana)", "<q>Fancy</q> večerja (oba se fejt zrihtava)", "Greva se kopat", "Bowling", "Kino (film po izbiri)", "Večerja doma (kuhaš ti, jaz ne znam)", "Skupni sprehod",
-			"Učenje inštrumentov (kitara, bass, violina,...)", "Pridiii k meni :)", "Pridem k tebi :)", "Gledava Friendse (SKUPAJ V OBJEMI!!)", "Movie night (ne kino, domaaa!)", "kolesarjenjee (ges sicer neman biciklina... si sposoden)", "Come and lets just cuddle",
-			"Kuhajva skuper (to raje kda de možost ka va samiva doma xD)", "Game night (špilava Uno, Človek ne jezi se, Twister,...)", "Kar koli ti poveš delava ;)", "Wee goooo shopping (tude če nič ne kupeva, čudno se obličeva pa poslikava)",
-			"Sladki date (samo sladice dovoljene!)", "Sprehod z Max-om (fejst de vesele)", "Izlet v gore (leko tudi bole kakšni hrib)", "Worship/Bible time together", "Pijača/druženje s skupnimi prijatelji", "Počivajva skuper (nap&cuddle)",
-			"Obiščeva mesto, ki ga še nesva(idk... Petanjci pa gledava Muro xD)", "Masaža date (masiranje obvezno za oba, ti leko malo duže)", "Ustvarjajva skupaj(riševa? Nevem to bole tvoje področje &#128513;)"];
-		var rand_date = Math.floor(Math.random() * date.length);
-		document.getElementById('result').innerHTML = "<h2>" + date[rand_date] + "</h2>";
+	function countdown() {
+	
+	var now = new Date();
+	var eventDate = new Date(2023, 11, 1);
+	var currentTime = now.getTime();
+	var evenTime = eventDate.getTime();
+	
+	var remTime = evenTime - currentTime;
+	
+	var sec = Math.floor(remTime / 1000);
+	var min = Math.floor(sec / 60);
+	var hur = Math.floor(min / 60);
+	var day = Math.floor(hur / 24);
+	
+	 hur %= 24;
+	 min %= 60;
+	 sec %= 60;
+
+	
+	$('.seconds').text(sec);
+	$('.minutes').text(min);
+	$('.hours').text(hur);
+	$('.days').text(day);
+	
+	setTimeout(countdown, 1000);
 	}
-	else{
-		swal({
-			title: "OPOZORILOOOOOO",
-			text: "Dovoljeni so samo trije poskusi!",
-			icon: "warning",
-			buttons: false
-		});
-	}
-}
+	
+	countdown();
+	});
